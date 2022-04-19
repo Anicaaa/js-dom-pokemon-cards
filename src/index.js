2acae3b181034845c7bf77c2006538e0d6037271
Anica Rahman <anicarahman2009@yahoo.it>
2022-04-19 17:32:57 +0100

    Added li, h2, img

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-
 console.log(data);
 
-//You can start simple and just render a single 
-//pokemon card from the first element
+/* You can start simple and just render a single 
+pokemon card from the first element */
+
+// Loop through data array of pokemon data
+const cardListUl = document.getElementById("card-list");
+for (let i = 0; i < data.length; i++) {
+  console.log("Pokemon at i", data[i]);
+  const pokemon = data[i];
+
+  // add li to ul
+  const cardLi = document.createElement("li");
+  cardLi.setAttribute("class", "card");
+  cardListUl.append(cardLi);
+
+  // create an h2
+  const cardH2 = document.createElement("h2");
+  cardH2.setAttribute("class", "card--title");
+  // set the text to pokemon name
+  cardH2.innerHTML = pokemon.name;
+  // add h2 to li
+  cardLi.append(cardH2);
+
+  // create an image
+  const cardImg = document.createElement("img");
+  cardImg.setAttribute("class", "card--img");
+  cardImg.width = "256";
+  cardImg.src = pokemon.sprites.other["official-artwork"].front_default;
+  cardLi.append(cardImg);
+}
+
 console.log(data[0]);
